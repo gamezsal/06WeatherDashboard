8e3dd0f374f91642c56be8e0099592857f7d40c9
Sal Gamez <gamezsalvador@gmail.com>
2020-10-21 21:26:49 -0600

    added logic take search area info and return api get call

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-var searchData = "London";
+  
+var searchData = "";
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchData + "&appid=6ec271a7eaa197efb35f9b736da2f3eb";
+//API GET function to the weather channel
+function citySearch(searchData) {
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchData + "&appid=6ec271a7eaa197efb35f9b736da2f3eb";
 
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
+} 
 
-$.ajax({
-    url:queryURL,
-    method:"GET"
-}).then(function(response){
-    console.log(response)
-})
+//Return results from search parameter when seached is clicked. 
+$("#searchBtn").on("click", function (e) {
+  e.preventDefault();
+  searchData = $("#searchInput").val();
+  citySearch(searchData);
+  console.log(searchData);
+});
+
+citySearch();
+

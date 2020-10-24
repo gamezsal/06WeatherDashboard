c6ef36653013c169a65a7f121c6671661dc40383
Sal Gamez <gamezsalvador@gmail.com>
2020-10-24 13:40:04 -0600

    Change javascript based on group study session

--- script.js
+++ script.js
@@ -1,33 +1,72 @@
-var searchData = "";
+var weatherSearch = "";
+//Gets Date from JavaScript
+var year = new Date().getFullYear();
+var month = new Date().getMonth() + 1;
+var day = new Date().getDate();
+$("#searchButton").on("click", function () {
+  weatherSearch = $("#weather-input").val().trim();
+  console.log(weatherSearch);
+  callWeather();
+  callFiveDay();
+})
 
-//API GET function to the weather channel
-function citySearch(searchData) {
-  var city = $(this).attr("city-name");
-  var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    searchData +
-    "&appid=6ec271a7eaa197efb35f9b736da2f3eb";
-
+function callWeather() {
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + weatherSearch + "&appid=f08e0e7293a9c130de4d782ff1db313d";
   $.ajax({
     url: queryURL,
-    method: "GET",
+    method: "GET"
   }).then(function (response) {
-    var cityDiv = $("<div class='card'>");
-    var returnMain = response.weather;
-    var cityOne = $("<li>").text("City Name: " + returnMain);
-    cityDiv.append(cityOne);
-
-    console.log(response.weather);
-    citySearch();
-  });
+    //Empties the weather-append div when button is pressed
+    $("#weather-append").empty();
+    //Converts to Fahrenheit from Kelvin
+    var fahrenheit = ((parseInt((response.main.temp) - 273.15) * 9 / 5 + 32)).toFixed();
+    //Grabs Humidity from response
+    var humidity = response.main.humidity + "%";
+    //Grabs wind from response
+    var wind = response.wind.speed;
+    //Creates the card for the current weather
+    var cardBody = $("<div>").addClass("card-body");
+    var cardTitle = $("<h3>").addClass("card-title").text(response.name + " " + month + "/" + day + "/" + year);
+    var cardTemp = $("<p>").text("Temperature: " + fahrenheit);
+    var cardHumidity = $("<p>").text("Humidity: " + humidity);
+    var cardWind = $("<p>").text("Wind Speed: " + wind);
+    //Appends Weather to the page
+    $("#weather-append").append(cardBody, cardTitle, cardTemp, cardHumidity, cardWind);
+    callUVIndex(response.coord.lat, response.coord.lon);
+  })
 }
-
-//Return results from search parameter when seached is clicked.
-$("#searchBtn").on("click", function (e) {
-  e.preventDefault();
-  searchData = $("#searchInput").val();
-  citySearch(searchData);
-  console.log(searchData);
-});
-
-
+//New API call for the UV index
+function callUVIndex(lat, lon) {
+  var queryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=f08e0e7293a9c130de4d782ff1db313d"
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    var uv = response.value;
+    console.log(uv);
+    var cardUV = $("<p>").text("UV Index: " + uv);
+    $("#weather-append").append(cardUV);
+  })
+}
+function callFiveDay () {
+  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + weatherSearch + "&appid=f08e0e7293a9c130de4d782ff1db313d"
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    var dayArray = response.list;
+    for (var i = 0; i < 5; i++) {
+      var dayIndex = dayArray[i];
+      var dayIncrement = day++;
+      var fahrenheit = ((parseInt((dayIndex.main.temp) - 273.15) * 9 / 5 + 32)).toFixed();
+      var cardBody = $("<div>").addClass("card-body");
+      var cardTitle = $("<h3>").addClass("card-title").text(month + "/" + dayIncrement + "/" + year);
+      var cardTemp = $("<p>").text("Temperature: " + fahrenheit);
+      $("#fiveday-append").append(cardBody, cardTitle, cardTemp);
+      console.log(dayIndex);
+      console.log(dayIncrement);
+    }
+  })
+}
